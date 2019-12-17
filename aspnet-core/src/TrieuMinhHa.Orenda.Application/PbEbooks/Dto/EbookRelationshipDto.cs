d98185649d5e6a3ff879b68de066521d5e611197
hungdo <Eleisure@DESKTOP-15I2I42>
2019-12-17 13:07:37 +0700

    hehe

---
+++ aspnet-core/src/TrieuMinhHa.Orenda.Application/PbEbooks/Dto/EbookRelationshipDto.cs
@@ -0,0 +1,21 @@
+﻿using Abp.Application.Services.Dto;
+using Abp.AutoMapper;
+using Abp.Domain.Entities.Auditing;
+using System;
+using TrieuMinhHa.Orenda.Authorization.EbookRelationship;
+
+namespace TrieuMinhHa.Orenda.PbEbooks.Dto
+{
+    [AutoMap(typeof(PbClass))]
+    public class PbClassListDto : FullAuditedEntity
+    {
+        public virtual string ClassGroup { get; set; }
+        public virtual string ClassName { get; set; }
+    }
+    [AutoMap(typeof(PbPlace))]
+    public class PbPlaceListDto : FullAuditedEntity
+    {
+        public virtual string PlaceGroup { get; set; }
+        public virtual string PlaceName { get; set; }
+    }
+}
