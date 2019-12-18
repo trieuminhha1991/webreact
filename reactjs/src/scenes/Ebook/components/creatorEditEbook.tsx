ccd80d5da71c61a2f332fd09bfa656e3571a43c0
trieuminhha1991 <namviet25791@gmail.com>
2019-12-18 23:47:48 +0700

    hehehe

--- reactjs/src/scenes/Ebook/components/creatorEditEbook.tsx
+++ reactjs/src/scenes/Ebook/components/creatorEditEbook.tsx
@@ -1,45 +1,103 @@
-import RoleStore from "../../../stores/roleStore";
 import {GetAllPermissionsOutput} from "../../../services/role/dto/getAllPermissionsOutput";
 import * as React from "react";
-import {Button} from "primereact/button";
-import {Dialog} from 'primereact/dialog';
-import {InputText} from "primereact/inputtext";
+import {Form, Input, Modal, Tabs} from "antd";
+import {FormComponentProps} from "antd/lib/form";
+import EbookStore from "../../../stores/ebookStore";
+import {L} from "../../../lib/abpUtility";
+import rules from "../../Roles/components/createOrUpdateRole.validation";
+import CheckboxGroup from 'antd/lib/checkbox/Group';
+import FormItem from "antd/lib/form/FormItem";
 
-export interface ICreateOrUpdateEbook{
-    item: RoleStore;
+
+const TabPane = Tabs.TabPane;
+export interface ICreateOrUpdateEbookProps extends FormComponentProps {
+    ebookStore: EbookStore;
     visible: boolean;
     onCancel: () => void;
     modalType: string;
     onOk: () => void;
     permissions: GetAllPermissionsOutput[];
-    havaId:boolean,
 }
-class CreateOrUpdateRole extends React.Component<ICreateOrUpdateEbook>{
-    state={
-        visible:true,
-        value: ''
-    }
-    onHide() {
-        this.setState({visible: false});
-    }
-    render(){
-        const footer = (
-            <div>
-                <Button label="Yes" icon="pi pi-check" onClick={this.onHide} />
-                <Button label="No" icon="pi pi-times" onClick={this.onHide} className="p-button-secondary" />
-            </div>
-        );
+class CreateOrUpdateREbook extends React.Component<ICreateOrUpdateEbookProps>{
+    state = {
+        confirmDirty: false,
+    };
+    render() {
+        const { permissions } = this.props;
+
+        const options = permissions.map((x: GetAllPermissionsOutput) => {
+            return { label: x.displayName, value: x.name };
+        });
+
+        const formItemLayout = {
+            labelCol: {
+                xs: { span: 6 },
+                sm: { span: 6 },
+                md: { span: 6 },
+                lg: { span: 6 },
+                xl: { span: 6 },
+                xxl: { span: 6 },
+            },
+            wrapperCol: {
+                xs: { span: 18 },
+                sm: { span: 18 },
+                md: { span: 18 },
+                lg: { span: 18 },
+                xl: { span: 18 },
+                xxl: { span: 18 },
+            },
+        };
+
+        const tailFormItemLayout = {
+            labelCol: {
+                xs: { span: 6 },
+                sm: { span: 6 },
+                md: { span: 6 },
+                lg: { span: 6 },
+                xl: { span: 6 },
+                xxl: { span: 6 },
+            },
+            wrapperCol: {
+                xs: { span: 18 },
+                sm: { span: 18 },
+                md: { span: 18 },
+                lg: { span: 18 },
+                xl: { span: 18 },
+                xxl: { span: 18 },
+            },
+        };
+
+        const { getFieldDecorator } = this.props.form;
+
         return (
-            <Dialog header="Godfather I" visible={this.state.visible} style={{width: '500px'}} footer={footer}
-                    onHide={this.onHide} maximizable>
-                <div className="p-grid">
-                    <div className="p-lg-4 p-md-12">Ebook Name *</div>
-                    <div className="p-lg-8 p-md-12">
-                        <InputText value={this.state.value} onChange={(e) => this.setState({value: (e.target as HTMLInputElement).value})} validateOnly={true}/>
-                    </div>
-                </div>
-            </Dialog>
-        )
+            <Modal
+                visible={this.props.visible}
+                cancelText={L('Cancel')}
+                okText={L('OK')}
+                onCancel={this.props.onCancel}
+                title={L('Role')}
+                onOk={this.props.onOk}
+            >
+                <Tabs defaultActiveKey={'role'} size={'small'} tabBarGutter={64}>
+                    <TabPane tab={L('RoleDetails')} key={'role'}>
+                        <FormItem label={L('RoleName')} {...formItemLayout}>
+                            {getFieldDecorator('name', { rules: rules.name })(<Input />)}
+                        </FormItem>
+                        <FormItem label={L('DisplayName')} {...formItemLayout}>
+                            {getFieldDecorator('displayName', { rules: rules.displayName })(<Input />)}
+                        </FormItem>
+                        <FormItem label={L('Description')} {...formItemLayout}>
+                            {getFieldDecorator('description')(<Input />)}
+                        </FormItem>
+                    </TabPane>
+                    <TabPane tab={L('RolePermission')} key={'permission'}>
+                        <FormItem {...tailFormItemLayout}>
+                            {getFieldDecorator('grantedPermissions', { valuePropName: 'value' })(<CheckboxGroup options={options} />)}
+                        </FormItem>
+                    </TabPane>
+                </Tabs>
+            </Modal>
+        );
     }
 }
-export default CreateOrUpdateRole;
+export default Form.create<ICreateOrUpdateEbookProps>()(CreateOrUpdateREbook);
